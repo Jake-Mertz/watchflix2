e283aa39a60dd8ff1b623210b58c90ebf1f92bab
Jake-Mertz <jakemertz101@gmail.com>
2020-12-22 16:21:58 -0500

    movie-list component Hello World

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import MovieList from './movie-list';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      message: null,
-      isLoading: true
+      // message: null,
+      // isLoading: true
+      view: {
+        name: 'movie-list',
+        params: {}
+      }
     };
   }
 
@@ -18,8 +23,17 @@ export default class App extends React.Component {
   }
 
   render() {
-    return this.state.isLoading
-      ? <h1>Testing connections...</h1>
-      : <h1>{this.state.message}</h1>;
+    // return this.state.isLoading
+    //   ? <h1>Testing connections...</h1>
+    //   : <h1>{this.state.message}</h1>;
+    let appView = null;
+    if (this.state.view.name === 'movie-list') {
+      appView = <MovieList
+        setView={this.serView}
+      />;
+    }
+    return (
+      <div>{appView}</div>
+    );
   }
 }
---
+++ client/components/movie-list.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class MovieList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: []
+    };
+  }
+
+  render() {
+    return (<div>Hello World!</div>);
+  }
+}
+
+export default MovieList;
